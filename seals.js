f9866367bea408cb1e08119530229feb940d225e
yoshi <211856568+yoshiii-dev@users.noreply.github.com>
2025-07-18 15:05:32 +0000

    Create seals.js

---
+++ seals.js
@@ -0,0 +1,15 @@
+const einsatzleiterNamen = [
+    "ARCHER",
+    "CUBA",
+    "DELTA",
+    "FOX",
+    "GHOST",
+    "HAVOC",
+    "IVY",
+    "REAPER",
+    "SHADOW",
+    "SNOW",
+    "TANGO",
+    "WINTERS",
+    "WOLFFE"
+];
